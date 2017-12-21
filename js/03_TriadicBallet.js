ff47c1354c769d6dada13fda854ea0aadda996da
takafumi.ooyama <takafumi.ooyama@i-studio.co.jp>
2017-12-21 20:38:44 +0900

    fix sketch3

--- js/03_TriadicBallet.js
+++ js/03_TriadicBallet.js
@@ -1,13 +1,15 @@
 /* 03_TriadicBallet.js */
 
-var strokeColor = color(0);
-var hue = random(0, 360);
-var resolution = 100;
-
+var strokeColor;
+var hue;
+var resolution;
 
 function setup(){
   createCanvas(windowHeight, windowHeight, P2D);
   colorMode(HSB, 360, 100, 100, 100);
+  strokeColor = color(0, 100, 10, 20);
+  hue = random(0, 360);
+  resolution = random(2, 10);
   smooth();
   noFill();
   background(360);
@@ -16,42 +18,41 @@ function setup(){
 function draw(){
   changeColor();
   push();
-   translate(width/2,height/2);
-
-    //int circleResolution = (int)map(mouseX+100,0,height,2,100);
-    var circleResolution = resolution;
+    translate(width/2,height/2);
     var radius = (mouseX+mouseY)/2-width/2;
+    var circleResolution = resolution;
     var angle = TWO_PI/circleResolution;
-    strokeWeight(2);
+    strokeWeight(1);
     stroke(strokeColor);
-
   pop();
 
   beginShape();
-    for (var i=0; i<=circleResolution; i++){
+  stroke(strokeColor);
+    for (var i=0; i<circleResolution; i++){
       var x = mouseX + cos(angle*i) * radius;
       var y = mouseY + sin(angle*i) * radius;
       vertex(x, y);
     }
-  endShape();
+  endShape(CLOSE);
 }
 
 function changeColor() {
-  var rand = random(3);
+  var rand = parseInt(random(3));
   switch(rand){
   case 0:
-    strokeColor = color(hue, 100, 10, 10);
+    strokeColor = color(hue, 100, 10, 20);
     break;
   case 1:
-    strokeColor = color(hue, 100, 50, 10);
+    strokeColor = color(hue, 100, 50, 20);
     break;
   case 2:
-    strokeColor = color(hue, 100, 100, 10);
+    strokeColor = color(hue, 100, 100, 20);
     break;
   }
 }
+
 function keyReleased(){
-  if (key == DELETE || key == BACKSPACE) {
+  if (key === " ") {
     hue = random(0, 360);
     resolution = random(2, 10);
     background(360);
